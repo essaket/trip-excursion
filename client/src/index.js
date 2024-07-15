debbfcad2e5c0a2774493e2f5a4824d95293ed07
essaket <hichamessaket17@gmail.com>
2024-07-15 15:21:17 +0100

    Add sing_up page

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Login from './pages/Login/login'; // Import the Landing-page component
+import Sign_up from './pages/Sign-up/sign-up'; // Import the Landing-page component
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <Login /> {/* Render the Landing-page component */}
+        <Sign_up /> {/* Render the Landing-page component */}
     </React.StrictMode>
 );
 
